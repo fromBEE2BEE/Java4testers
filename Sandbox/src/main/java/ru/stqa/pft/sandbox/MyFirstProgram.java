8deca862d355829181966ce051c6b1e82943cfc9
fromBEE2BEE <elenakorneychuk.mail@gmail.com>
2020-10-27 18:18:47 +0200

    Созд-е класса Point и функции для вычисл-я расстояния между 2мя точками

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,26 +1,28 @@
 package ru.stqa.pft.sandbox;
-
   public class MyFirstProgram {
     public static void main(String[] args) {
         hello("world");
         hello("user");
         hello("Alexei");
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+        Point p1 = new Point();
+        Point p2 = new Point();
+        System.out.println("Distance between p1 and p2 = "  + distance(p1, p2));
     }
-
-  public static void hello(String somebody) {
+    public static void hello(String somebody) {
       System.out.println("Hello," + somebody + "!");
-  }
-  public static double area(double len) {
-        return len*len;
-  }
-  public static double area(double a, double b) {
-        return a*b;
-  }
+    }
+    public static double distance(Point p1, Point p2) {
+      double x1 = 3;
+      double x2 = 7;
+      double y1 = 3;
+      double y2 = 6;
+      double a = x2 - x1;
+      double b = y2 - y1;
+      return Math.sqrt((b) * (b) + (a) * (a));
+    }
 }
