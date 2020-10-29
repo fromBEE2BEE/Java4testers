74f34c15bb607ef6bf3a54b507ca1dbdcc00804f
fromBEE2BEE <elenakorneychuk.mail@gmail.com>
2020-10-29 21:20:46 +0200

    Внесены исправления по задаче 2

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,12 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
+
     public static void main(String[] args) {
-      Point p1 = new Point(3.0, 3.0);
-      Point p2 = new Point(7.0, 6.0);
-      System.out.println("The dictance from Point p1 to Point p2 is " + p1.distance(p1, p2) + ".");
-    }
-  }
+        Point p1 = new Point(3,3);
+        Point p2 = new Point(7.0, 6.0);
+        //System.out.println("The dictance from Point p1 to Point p2 is " + p1.distance(p1, p2) + ".");
+        System.out.println("The dictance from Point p1 to Point p2 is " + p1.distance(p2));
+        }
+
+}
--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,16 +1,18 @@
 package ru.stqa.pft.sandbox;
 
   public class Point{
-    public double x, y;
+   public double x, y;
 
-    public Point(double x, double y){
-      this.x = x;
-      this.y = y;
+    public Point(double x1, double y1){
+      this.x = x1;
+      this.y = y1;
     }
-
-    public double distance(Point p1, Point p2){
-      double dx = p1.x - p2.x;
-      double dy = p1.y - p2.y;
-      return Math.sqrt(dx * dx + dy * dy);
+    //public double distance(Point p1, Point p2){
+     //double dx = p1.x - p2.x;
+     //double dy = p1.y - p2.y;
+     //return Math.sqrt(dx * dx + dy * dy);
+    //}
+    public double distance(Point p2){
+        return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
     }
   }
