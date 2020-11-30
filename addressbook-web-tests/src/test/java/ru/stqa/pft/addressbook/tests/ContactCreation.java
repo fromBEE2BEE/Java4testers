9a47e2f52a26364bb9d52c9e692deb595b0094b7
fromBEE2BEE <elenakorneychuk.mail@gmail.com>
2020-11-30 14:43:46 +0200

    Корректировки. Класс ContactData перенесен в пакет model.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreation.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreation.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.util.concurrent.TimeUnit;
 
