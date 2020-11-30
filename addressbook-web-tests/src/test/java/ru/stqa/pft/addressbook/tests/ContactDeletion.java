4f5c48404fb54c57df6a2d320b8d23cb6aec0e9a
fromBEE2BEE <elenakorneychuk.mail@gmail.com>
2020-11-30 14:30:45 +0200

    Параметризован метод Логин в тесте ContactDeletion

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletion.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletion.java
@@ -16,17 +16,17 @@ public class ContactDeletion {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
-    login();
+    login("admin", "secret");
 
   }
 
-  private void login() {
+  private void login(String username, String password) {
     wd.findElement(By.cssSelector("input[name=\"user\"]")).click();
     wd.findElement(By.cssSelector("input[name=\"user\"]")).clear();
-    wd.findElement(By.cssSelector("input[name=\"user\"]")).sendKeys("admin");
+    wd.findElement(By.cssSelector("input[name=\"user\"]")).sendKeys(username);
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
