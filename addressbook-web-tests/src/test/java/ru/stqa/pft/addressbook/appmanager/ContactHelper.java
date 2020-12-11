b87b292fe27eb24823e25c9f5692c7d9ef8194d7
fromBEE2BEE <elenakorneychuk.mail@gmail.com>
2020-12-11 11:35:37 +0200

    3.8 Создание доп.параметра boolean creation в методе fillContactForm(ContactHelper), добавление в тесты параметров true/false, корр-ки в ContactHelper(if creation)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -2,9 +2,9 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.util.concurrent.TimeUnit;
@@ -39,7 +39,7 @@ public class ContactHelper extends HelperBase {
     click(By.xpath(s));
   }
 
-  public void fillContactForm(ContactData contactData) {
+  public void fillContactForm(ContactData contactData, boolean creation) {
     type(By.name("firstname"), contactData.getFirstname());
     type(By.name("lastname"), contactData.getLastname());
     type(By.name("title"), contactData.getTitle());
@@ -48,8 +48,10 @@ public class ContactHelper extends HelperBase {
     type(By.name("work"), contactData.getTelephoneWork());
     type(By.name("email"), contactData.getEmail());
 
-    if (isElementPresent(By.name("new_group"))) {
+    if (creation) {
       new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
+    } else {
+      Assert.assertFalse(isElementPresent(By.name("new_group")));
     }
   }
 
