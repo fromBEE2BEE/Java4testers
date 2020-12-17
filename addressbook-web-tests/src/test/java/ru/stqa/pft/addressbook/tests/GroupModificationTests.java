f17850094012141fb9d4541bd9b72e843c9941c6
fromBEE2BEE <elenakorneychuk.mail@gmail.com>
2020-12-17 23:08:04 +0200

    3.10. Проверка и обеспечение выполнения предусловий. GroupModificationTests.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -8,6 +8,9 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("testQ1", null, null));
+    }
     app.getGroupHelper().selectGroup("selected[]");
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("testQ1", "testQ2", "testQ3"));
