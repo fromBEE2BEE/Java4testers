eec8626fef978b96562dae259c0f91a1574bb73f
fromBEE2BEE <elenakorneychuk.mail@gmail.com>
2020-12-24 01:02:47 +0200

    4.5. Формирование коллекции. All GroupTests.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupDeletionTests extends TestBase {
 
   @Test
@@ -13,15 +15,11 @@ public class GroupDeletionTests extends TestBase {
       app.getGroupHelper().createGroup(new GroupData("testQ1", null, null));
 
     }
-    int before = app.getGroupHelper().getGroupCount();
-    // удаление 1-го элемента:
-    //app.getGroupHelper().selectGroup(0);
-    // удаление последнего элемента:
-    app.getGroupHelper().selectGroup(before - 1);
-    //app.getGroupHelper().selectGroup("selected[]");
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before -1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() -1);
   }
 }
