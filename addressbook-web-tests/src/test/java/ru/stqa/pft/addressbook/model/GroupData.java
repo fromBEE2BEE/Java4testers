144a4c359889786a2a057bd338f80c892b1e7ca5
fromBEE2BEE <elenakorneychuk.mail@gmail.com>
2020-12-25 17:44:28 +0200

    4.8. Поиск максимального элемента в коллекции. GroupCreationTests.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,26 +1,28 @@
 package ru.stqa.pft.addressbook.model;
 
-import java.util.Objects;
-
 public class GroupData {
-  private final String id;
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
+  public int getId() {
+    return id;
+  }
+
   public String getName() {
     return name;
   }
@@ -33,6 +35,8 @@ public class GroupData {
     return footer;
   }
 
+
+
   @Override
   public String toString() {
     return "GroupData{" +
@@ -40,21 +44,27 @@ public class GroupData {
             ", name='" + name + '\'' +
             '}';
   }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
+
     GroupData groupData = (GroupData) o;
-    if (id != null ? !id.equals(groupData.id) : groupData.id !=null) return false;
-    return name !=null ? name.equals(groupData.name) : groupData.name == null;
+
+    if (id != groupData.id) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, name);
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
   }
-  public String getId() {
-    return id;
+
+
+
+  public void setId(int id) {
+    this.id = id;
   }
 }
